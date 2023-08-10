3aafdd3d08a1274d76dcede173285215fd7553e0
Lúcia Boutti <luboutti19@gmail.com>
2023-08-10 03:16:28 -0300

    pagina de sustentabilidade ta feita

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,24 +1,9 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+const contents = document.querySelectorAll(".content");
 
-
-// script para o slide de imagens
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  const slides = document.getElementsByClassName("slides");
-  
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  
-  slideIndex++;
-  
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 2000); // Muda a imagem a cada 2 segundos
-}
+collapsibles.forEach((collapsible, index) => {
+  collapsible.addEventListener("click", function() {
+    contents[index].classList.toggle("active");
+    collapsible.classList.toggle("active");
+  });
+});
